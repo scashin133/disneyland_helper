923f0e9c8a60cf48ff8b9a33696b65fdb54a35ee
Sean <scashin133@gmail.com>
2009-03-29 17:24:41 -0700

    Adding XHTML declarations.  Filling out page with a little more direction.  Adding stylesheets to pretty up the index page a bit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,18 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  
+  before_filter :set_title
+  
+  layout "default"
+  
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
+  
+  protected
+  
+  def set_title
+    @title = "Disneyland Helper"
+  end
+  
 end
